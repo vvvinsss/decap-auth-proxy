b6aae46b9e0141f95ea1ec5192f37890e205cfa0
vvvinsss <ljdui01@gmail.com>
2025-05-15 20:37:40 +0200

    Update server.js

--- server.js
+++ server.js
@@ -44,6 +44,7 @@ app.post('/auth', async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('OAuth proxy server running on port 3000');
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`OAuth proxy server running on port ${PORT
+
